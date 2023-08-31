80bd6267c79d42d3d99824ef68a1d5424aedc28c
sree-r-one <sreeraj_ec@hotmail.com>
2023-09-01 00:38:57 +0800

    until layout and position

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,93 @@ import warningImage from "./assets/warning.svg";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-red-300">Hello world!</h1>
+    <div className="container">
+      <h3>Heading</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
+        consequuntur non veniam tempora nisi beatae velit numquam deleniti
+        debitis temporibus voluptates minus neque dicta quo, minima ut alias
+        ducimus aliquam nostrum quidem ex iusto eaque earum. Eaque, dolore
+        incidunt ab quibusdam exercitationem amet ullam in. Velit autem placeat
+        odit nihil, et tenetur sed dolores dolore totam? Veniam praesentium
+        velit molestias, quos neque molestiae quis ad? Magni cumque earum
+        inventore nemo porro, quasi necessitatibus eligendi ea natus aut tenetur
+        molestiae dolorem cum, tempore animi rem possimus error ab atque enim
+        esse vero! Placeat, sapiente vitae velit qui quo maiores aliquam, libero
+        id ut repudiandae ratione nostrum. Nobis maiores delectus voluptate
+        similique ratione dolores aliquam doloremque veritatis facilis, eius
+        optio laudantium id quo, cupiditate, dicta distinctio libero pariatur
+        labore beatae quisquam illum amet ad! Qui, eum quas deserunt quae alias
+        dolorum inventore ut tenetur iure sint nemo consequatur fugiat illum
+        quaerat? Molestias sequi dolorum tempore, culpa aliquam quo magnam
+        asperiores accusantium vel hic? Dolorem saepe voluptates quam maiores
+        minus soluta quibusdam ducimus eius, placeat itaque, odit error
+        dignissimos sit, aliquam enim. Eum, temporibus distinctio aspernatur
+        sint vero deleniti quae assumenda, quam maiores voluptatum est omnis!
+        Quaerat, earum consectetur. Dolor impedit placeat repellendus.S
+      </p>
 
-      <div className="mx-auto flex  max-w-sm items-center space-x-4 rounded-xl bg-white p-6 shadow-lg">
-        <img src={warningImage} alt="" className="h-12 w-12" />
-        <div className="">
-          <h3 className="text-xl font-medium text-black">Are You Sure?</h3>
-          <p className="text-slate-400">You are about to delete a post</p>
-        </div>
+      <div className="mt-24 flex space-x-6">
+        <div>Item 1 </div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+        <div>Item 4</div>
+        <div>Item 5</div>
+        <div>Item 6</div>
+        <div>Item 7</div>
+        <div>Item 8</div>
       </div>
-      <input
-        className="accent-purple-400"
-        type="checkbox"
-        name=""
-        id=""
-        checked
-      />
+
+      <div className="mt-24 flex flex-col space-y-9">
+        <div>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+        <div>Item 4</div>
+        <div>Item 5</div>
+        <div>Item 6</div>
+        <div>Item 7</div>
+        <div>Item 8</div>
+        <div>Item 9</div>
+      </div>
+
+      <div className="flex items-stretch space-x-4">
+        <div className="bg-orange-200 py-4">Item 1</div>
+        <div className="bg-orange-200 py-12">Item 2</div>
+        <div className="bg-orange-200 py-8">Item 3</div>
+        <div className="bg-orange-200">Item 4</div>
+        <div className="bg-orange-200">Item 5</div>
+        <div className="bg-orange-200">Item 6</div>
+        <div className="bg-orange-200">Item 7</div>
+      </div>
+      <div className="flex items-start space-x-4">
+        <div className="bg-blue-200 py-4">Item 1</div>
+        <div className="bg-blue-200 py-12">Item 2</div>
+        <div className="bg-blue-200 py-8">Item 3</div>
+        <div className="bg-blue-200">Item 4</div>
+        <div className="bg-blue-200">Item 5</div>
+        <div className="bg-blue-200">Item 6</div>
+        <div className="bg-blue-200">Item 7</div>
+      </div>
+      <div className="flex items-end space-x-4">
+        <div className="bg-red-200 py-4">Item 1</div>
+        <div className="bg-red-200 py-12">Item 2</div>
+        <div className="bg-red-200 py-8">Item 3</div>
+        <div className="bg-red-200">Item 4</div>
+        <div className="bg-red-200">Item 5</div>
+        <div className="bg-red-200">Item 6</div>
+        <div className="bg-red-200">Item 7</div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <div className="bg-orange-200 py-4">Item 1</div>
+        <div className="bg-orange-200 py-12">Item 2</div>
+        <div className="bg-orange-200 py-8">Item 3</div>
+        <div className="bg-orange-200">Item 4</div>
+        <div className="bg-orange-200">Item 5</div>
+        <div className="bg-orange-200">Item 6</div>
+        <div className="bg-orange-200">Item 7</div>
+      </div>
+
+      <div className="h-screen w-screen  bg-blue-300 text-center">Hero</div>
     </div>
   );
 };
