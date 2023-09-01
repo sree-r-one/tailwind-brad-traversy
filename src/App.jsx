08acc5c112726dd318f3b2b02719363bb0b6fd5c
sree-r-one <sreeraj_ec@hotmail.com>
2023-09-02 01:43:28 +0800

    adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,17 @@
 import React from "react";
-import warningImage from "./assets/warning.svg";
 
 const App = () => {
   return (
-    <div className="container">
-      <h3>Heading</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
-        consequuntur non veniam tempora nisi beatae velit numquam deleniti
-        debitis temporibus voluptates minus neque dicta quo, minima ut alias
-        ducimus aliquam nostrum quidem ex iusto eaque earum. Eaque, dolore
-        incidunt ab quibusdam exercitationem amet ullam in. Velit autem placeat
-        odit nihil, et tenetur sed dolores dolore totam? Veniam praesentium
-        velit molestias, quos neque molestiae quis ad? Magni cumque earum
-        inventore nemo porro, quasi necessitatibus eligendi ea natus aut tenetur
-        molestiae dolorem cum, tempore animi rem possimus error ab atque enim
-        esse vero! Placeat, sapiente vitae velit qui quo maiores aliquam, libero
-        id ut repudiandae ratione nostrum. Nobis maiores delectus voluptate
-        similique ratione dolores aliquam doloremque veritatis facilis, eius
-        optio laudantium id quo, cupiditate, dicta distinctio libero pariatur
-        labore beatae quisquam illum amet ad! Qui, eum quas deserunt quae alias
-        dolorum inventore ut tenetur iure sint nemo consequatur fugiat illum
-        quaerat? Molestias sequi dolorum tempore, culpa aliquam quo magnam
-        asperiores accusantium vel hic? Dolorem saepe voluptates quam maiores
-        minus soluta quibusdam ducimus eius, placeat itaque, odit error
-        dignissimos sit, aliquam enim. Eum, temporibus distinctio aspernatur
-        sint vero deleniti quae assumenda, quam maiores voluptatum est omnis!
-        Quaerat, earum consectetur. Dolor impedit placeat repellendus.S
-      </p>
+    <div>
+      <h1>React App</h1>
 
-      <div className="mt-24 flex space-x-6">
-        <div>Item 1 </div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
-        <div>Item 5</div>
-        <div>Item 6</div>
-        <div>Item 7</div>
-        <div>Item 8</div>
+      <div className="relative h-12 w-1/2 bg-red-300">
+        hello
+        <p>parent element</p>
+        <div className="absolute bottom-0 right-0 bg-red-500">
+          <p>absolute</p>
+        </div>
       </div>
-
-      <div className="mt-24 flex flex-col space-y-9">
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
-        <div>Item 5</div>
-        <div>Item 6</div>
-        <div>Item 7</div>
-        <div>Item 8</div>
-        <div>Item 9</div>
-      </div>
-
-      <div className="flex items-stretch space-x-4">
-        <div className="bg-orange-200 py-4">Item 1</div>
-        <div className="bg-orange-200 py-12">Item 2</div>
-        <div className="bg-orange-200 py-8">Item 3</div>
-        <div className="bg-orange-200">Item 4</div>
-        <div className="bg-orange-200">Item 5</div>
-        <div className="bg-orange-200">Item 6</div>
-        <div className="bg-orange-200">Item 7</div>
-      </div>
-      <div className="flex items-start space-x-4">
-        <div className="bg-blue-200 py-4">Item 1</div>
-        <div className="bg-blue-200 py-12">Item 2</div>
-        <div className="bg-blue-200 py-8">Item 3</div>
-        <div className="bg-blue-200">Item 4</div>
-        <div className="bg-blue-200">Item 5</div>
-        <div className="bg-blue-200">Item 6</div>
-        <div className="bg-blue-200">Item 7</div>
-      </div>
-      <div className="flex items-end space-x-4">
-        <div className="bg-red-200 py-4">Item 1</div>
-        <div className="bg-red-200 py-12">Item 2</div>
-        <div className="bg-red-200 py-8">Item 3</div>
-        <div className="bg-red-200">Item 4</div>
-        <div className="bg-red-200">Item 5</div>
-        <div className="bg-red-200">Item 6</div>
-        <div className="bg-red-200">Item 7</div>
-      </div>
-      <div className="flex items-center space-x-4">
-        <div className="bg-orange-200 py-4">Item 1</div>
-        <div className="bg-orange-200 py-12">Item 2</div>
-        <div className="bg-orange-200 py-8">Item 3</div>
-        <div className="bg-orange-200">Item 4</div>
-        <div className="bg-orange-200">Item 5</div>
-        <div className="bg-orange-200">Item 6</div>
-        <div className="bg-orange-200">Item 7</div>
-      </div>
-
-      <div className="h-screen w-screen  bg-blue-300 text-center">Hero</div>
     </div>
   );
 };
