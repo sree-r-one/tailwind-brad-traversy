85836eda08c3164fb7c6ebb6185f50907d7063c1
sree-r-one <sreeraj_ec@hotmail.com>
2023-09-03 00:34:16 +0800

    until columns

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,27 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>React App</h1>
-
-      <div className="relative h-12 w-1/2 bg-red-300">
-        hello
-        <p>parent element</p>
-        <div className="absolute bottom-0 right-0 bg-red-500">
-          <p>absolute</p>
-        </div>
-      </div>
+      App
+      <a
+        href=""
+        className="group block max-w-xs rounded-lg bg-white p-6 shadow hover:bg-sky-500"
+      >
+        <h3 className="group-hover:text-white">Card Title</h3>
+        <p>This is the card text</p>
+      </a>
+      {/* Pseudo Classes */}
+      <ul>
+        <li className="even:bg-red-200">Item 1</li>
+        <li className="even:bg-red-200">Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+        <li>Item 6</li>
+      </ul>
+      <select name="" id="">
+        <option value="Yes">Yes</option>
+        <option value="No">No</option>
+      </select>
     </div>
   );
 };
